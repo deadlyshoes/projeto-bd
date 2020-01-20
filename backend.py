8d5685dd4797b135e6b686959eb85a43d04aa8fb
hildemar <khoseilmar@gmail.com>
2020-01-20 10:26:52 -0300

    registro usando txt

--- backend.py
+++ backend.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
+
+class Usuario():
+	def __init__(self, usuario, senha):
+		self.usuario = usuario
+		self.senha = senha
+	
 	
 @app.route("/", methods=['GET', 'POST'])
 def login():
@@ -11,6 +17,15 @@ def login():
 		else:
 			return redirect(url_for('planetas'))
 	return render_template("login.html")
+
+@app.route("/registrar", methods=['GET', 'POST'])
+def registro():
+	if request.method == 'POST':
+		info = [request.form["username"], request.form["password"]]
+		with open("data.txt", 'w') as f:
+			f.write(" ".join(info))
+		return render_template("planetas.html")
+	return render_template("registro.html")
 
 @app.route("/planetas")
 def planetas():
