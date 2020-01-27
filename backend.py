b53829241c3b0bea652930e3af89236094737c04
hildemar <khoseilmar@gmail.com>
2020-01-27 13:53:37 -0300

    update adicionar

--- backend.py
+++ backend.py
@@ -6,27 +6,23 @@ class Usuario():
 	def __init__(self, usuario, senha):
 		self.usuario = usuario
 		self.senha = senha
-	
+		
 	
 @app.route("/", methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
 		usuario = request.form["username"]
 		senha = request.form["password"]
-		return render_template("planetas.html")	 
+		return render_template("entidades.html")	 
 	return render_template("login.html")
 
-@app.route("/registrar", methods=['GET', 'POST'])
-def registro():
-	if request.method == 'POST':
-		usuario = request.form["username"]
-		senha = request.form["password"]
-		return render_template("planetas.html")
-	return render_template("registro.html")
-
-@app.route("/planetas")
-def planetas():
-	return render_template("planetas.html")
+@app.route("/entidades")
+def entidades():
+	return render_template("entidades.html")
+	
+@app.route("/add_planeta")
+def add_planeta():
+	return render_template("add_planeta.html")
 
 if __name__ == "__main__":
 	app.run(threaded=True, port=5000)
