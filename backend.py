84df4409b9e5e39dc9d8cabf90632a6de07f5347
deadlyshoes <khoseilmar@gmail.com>
2020-01-29 22:41:07 -0300

    update

--- backend.py
+++ backend.py
@@ -2,7 +2,8 @@ from flask import Flask, jsonify, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 	
-lista = ["0", "1"]
+lista = [["0", "Terra", "Planeta", "Sistema Solar", "Via Láctea"], ["1", "Marte", "Planeta", "Sistema Solar", "Via Láctea"]]
+lista_info = ["id", "nome", "tipo", "sistema", "galáxia"]
 	
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -32,8 +33,9 @@ def entidades():
 
 @app.route("/entidades/infos/<i>", methods=["GET", "POST"])
 def infos(i):
-	iden = lista[0]
-	return render_template("infos.html", iden=iden)
+	print(i)
+	iden = lista[int(i)]
+	return render_template("infos.html", sub=lista[int(i)], inf=lista_info)
 
 @app.route("/adicionar", methods=["GET", "POST"])
 def adicionar():
@@ -42,6 +44,14 @@ def adicionar():
 		#tipo = request.form["tipo"]
 		return redirect("entidades")
 	return render_template("adicionar.html")
+	
+@app.route("/modificar", methods=["GET", "POST"])
+def modificar():
+	if request.method == "POST":
+		nome = request.form["nome"]
+		#tipo = request.form["tipo"]
+		return redirect("entidades")
+	return render_template("modificar.html")
 
 if __name__ == "__main__":
 	app.run(threaded=True, debug=True, port=5000)
