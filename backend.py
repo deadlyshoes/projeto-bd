9b62b520fb7b23d07d4ee3627f978ea7092e2e21
deadlyshoes <khoseilmar@gmail.com>
2020-03-15 21:36:00 -0300

    Consertar indentação

--- backend.py
+++ backend.py
@@ -7,76 +7,76 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Galaxia(db.Model):
-    __tablename__ = 'galaxia'
-    id = db.Column('id_galaxia', db.Integer, primary_key=True)
-    nome = db.Column('nome', db.Unicode)
-    qt_sistema = db.Column('qt_sistema', db.Integer)
-    dist_terra = db.Column('dist_terra', db.Float)
+	__tablename__ = 'galaxia'
+	id = db.Column('id_galaxia', db.Integer, primary_key=True)
+	nome = db.Column('nome', db.Unicode)
+	qt_sistema = db.Column('qt_sistema', db.Integer)
+	dist_terra = db.Column('dist_terra', db.Float)
 
-    def __init__(self, nome, qt_sistema, dist_terra):
-        self.nome = nome
+	def __init__(self, nome, qt_sistema, dist_terra):
+		self.nome = nome
 
-    def infos(self):
-        return {"id": self.id, "Nome": self.nome, "Quantidade sistema": self.qt_sistema, "Distância até a terra": self.dist_terra}
+	def infos(self):
+		return {"id": self.id, "Nome": self.nome, "Quantidade sistema": self.qt_sistema, "Distância até a terra": self.dist_terra}
 
 class Sistema(db.Model):
-    __tablename__ = 'sistema'
-    id = db.Column('id_sistema', db.Integer, primary_key=True)
-    nome = db.Column('nome', db.Unicode)
-    qt_planetas = db.Column('qt_planetas', db.Integer)
-    qt_estrelas = db.Column('qt_estrelas', db.Integer)
-    idade = db.Column('idade', db.Integer)
+	__tablename__ = 'sistema'
+	id = db.Column('id_sistema', db.Integer, primary_key=True)
+	nome = db.Column('nome', db.Unicode)
+	qt_planetas = db.Column('qt_planetas', db.Integer)
+	qt_estrelas = db.Column('qt_estrelas', db.Integer)
+	idade = db.Column('idade', db.Integer)
 
-    def __init__(self, nome):
-        self.nome = nome
+	def __init__(self, nome):
+		self.nome = nome
 
-    def infos(self):
-        return {"id": self.id, "Nome": self.nome, "Quantidade de planetas": self.qt_planetas, "Quantidade de estrelas": self.qt_estrelas, "Idade": self.idade}
+	def infos(self):
+		return {"id": self.id, "Nome": self.nome, "Quantidade de planetas": self.qt_planetas, "Quantidade de estrelas": self.qt_estrelas, "Idade": self.idade}
 
 class Estrela(db.Model):
-    __tablename__ = 'estrela'
-    id = db.Column('id_estrela', db.Integer, primary_key=True)
-    nome = db.Column('nome', db.Unicode)
-    tamanho = db.Column('tamanho', db.Float)
-    idade = db.Column('idade', db.Integer)
-    possui_estrela = db.Column('possui_estrela', db.Boolean)
-    dist_terra = db.Column('dist_terra', db.Float)
+	__tablename__ = 'estrela'
+	id = db.Column('id_estrela', db.Integer, primary_key=True)
+	nome = db.Column('nome', db.Unicode)
+	tamanho = db.Column('tamanho', db.Float)
+	idade = db.Column('idade', db.Integer)
+	possui_estrela = db.Column('possui_estrela', db.Boolean)
+	dist_terra = db.Column('dist_terra', db.Float)
 
-    def __init__(self, nome):
-        self.nome = nome
+	def __init__(self, nome):
+		self.nome = nome
 
-    def infos(self):
-        return {"id": self.id, "Nome": self.nome, "Tamanho": self.tamanho, "Idade": self.idade, "Possui estrela", self.possui_estrela, "Distância até a terra": self.dist_terra}
+	def infos(self):
+		return {"id": self.id, "Nome": self.nome, "Tamanho": self.tamanho, "Idade": self.idade, "Possui estrela", self.possui_estrela, "Distância até a terra": self.dist_terra}
 
 class Planeta(db.Model):
 	__tablename__ = 'planeta'
 	id = db.Column('id_planeta', db.Integer, primary_key=True)
 	nome = db.Column('nome', db.Unicode)
-    tamanho = db.Column('tamanho', db.Float)
-    peso = db.Column('peso', db.Float)
-    vel_rotacao = db.Column('vel_rotacao', db.Float)
-    possui_sn = db.Column('possui_sn', db.Boolean)
-    comp_planeta = db.Column('comp_planeta', db.Unicode)
+	tamanho = db.Column('tamanho', db.Float)
+	peso = db.Column('peso', db.Float)
+	vel_rotacao = db.Column('vel_rotacao', db.Float)
+	possui_sn = db.Column('possui_sn', db.Boolean)
+	comp_planeta = db.Column('comp_planeta', db.Unicode)
 	
 	def __init__(self, nome):
 		self.nome = nome
 		
 	def infos(self):
-        return {"id": self.id, "Nome": self.nome, "Tamanho": self.tamanho, "Peso": self.peso, "Velocidade de rotação": self.vel_rotacao, "Possui satélite natural": self.possui_sn, "Composição do planeta": self.comp_planeta}
+		return {"id": self.id, "Nome": self.nome, "Tamanho": self.tamanho, "Peso": self.peso, "Velocidade de rotação": self.vel_rotacao, "Possui satélite natural": self.possui_sn, "Composição do planeta": self.comp_planeta}
 
 class Satelite(db.Model):
 	__tablename__ = 'satelite'
 	id = db.Column('id_satelite', db.Integer, primary_key=True)
 	nome = db.Column('nome', db.Unicode)
-    tamanho = db.Column('tamanho', db.Float)
-    peso = db.Column('peso', db.Float)
-    comp_sn = db.Column('comp_sn', db.Unicode)
+	tamanho = db.Column('tamanho', db.Float)
+	peso = db.Column('peso', db.Float)
+	comp_sn = db.Column('comp_sn', db.Unicode)
 	
 	def __init__(self, nome):
 		self.nome = nome
 		
 	def infos(self):
-        return {"id": self.id, "Nome": self.nome, "Tamanho": self.tamanho, "Peso": self.peso, "Composição": self.comp_sn}
+		return {"id": self.id, "Nome": self.nome, "Tamanho": self.tamanho, "Peso": self.peso, "Composição": self.comp_sn}
 
 @app.route("/", methods=["GET", "POST"])
 def login():
@@ -117,18 +117,18 @@ def adicionar():
 	if request.method == "POST":
 		nome = request.form["nome"]
 		tipo = request.form["tipo"]
-        
+		
 		if tipo == "galaxia":
 			db.session.add(Galaxia(nome))
-        else if tipo == "sistema":
+		else if tipo == "sistema":
 			db.session.add(Sistema(nome))
-        else if tipo == "estrela":
-            db.session.add(Estrela(nome))
-        else if tipo == "planeta":
-            db.session.add(Planeta(nome))
-        else:
-            db.session.add(Satelite(nome))
-        
+		else if tipo == "estrela":
+			db.session.add(Estrela(nome))
+		else if tipo == "planeta":
+			db.session.add(Planeta(nome))
+		else:
+			db.session.add(Satelite(nome))
+		
 		db.session.commit()
 		return redirect("entidades")
 	return render_template("adicionar.html")
