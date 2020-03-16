90538bce9872bc7b196a3407c4d99dd149522fe2
deadlyshoes <khoseilmar@gmail.com>
2020-03-15 21:48:40 -0300

    SQL: Adicionar atributos restantes de planeta

--- entidades.sql
+++ entidades.sql
@@ -3,5 +3,10 @@ create sequence planeta_seq start 1 increment 1;
 create table planeta(
 	id_planeta int default nextval('planeta_seq'),
 	nome varchar(45),
+    tamanho float,
+    peso float,
+    vel_rotacao float,
+    possui_sn bool,
+    comp_planeta varchar(45),
 	constraint pk_planeta primary key (id_planeta)
 );
