6e52fd031fda685d38c1ddbf34c55aee3727a704
deadlyshoes <khoseilmar@gmail.com>
2020-03-21 20:51:27 -0300

    Utilizar id customizado em todas as entidades

--- entidades.sql
+++ entidades.sql
@@ -1,4 +1,8 @@
+create sequence galaxia_seq start 1 increment 1;
+create sequence sistema_seq start 1 increment 1;
+create sequence estrela_seq start 1 increment 1;
 create sequence planeta_seq start 1 increment 1;
+create sequence satelite_seq start 1 increment 1;
 
 create table planeta(
 	id_planeta varchar(45) default 'temp',
@@ -11,22 +15,8 @@ create table planeta(
 	constraint pk_planeta primary key (id_planeta)
 );
 
-create function proxima_chave() returns trigger as $definir_chave$
-    declare
-        pk varchar(45) := 'planeta';
-        run_seq_num integer := 30;
-    begin
-        select nextval('planeta_seq') into run_seq_num;
-        pk := pk || run_seq_num;
-        update planeta set id_planeta = pk where id_planeta = 'temp';
-        return new;
-    end;
-$definir_chave$ language plpgsql;
-
-create trigger definir_chave after insert on planeta for each row execute procedure proxima_chave()
-
 create table galaxia(
-	id_galaxia int,
+	id_galaxia varchar(45) default 'temp',
   	qt_sistema int,
   	dist_terra int,
   	nome varchar(45),
@@ -34,36 +24,18 @@ create table galaxia(
 );
 
 create table sistema(
-	id_sistema int,
+	id_sistema varchar(45) default 'temp',
   	nome varchar(45),
   	qt_planetas int,
   	qt_estrelas int,
   	idade int,
-  	galaxia int,
+  	galaxia varchar(45),
   	constraint pk_sistema primary key (id_sistema),
   	constraint fk_galaxia foreign key (galaxia) references galaxia(id_galaxia)
 );
 
-create table sistema_estrela(
-	sistema int,
-  	sistema_galaxia int,
-  	estrela int,
-  	constraint fk_sistema foreign key (sistema) references sistema(id_sistema),
-  	constraint fk_sistema_galaxia foreign key (sistema_galaxia) references galaxia(id_galaxia),
-  	constraint fk_estrela foreign key (estrela) references estrela(id_estrela)
-);
-
-create table sistema_planeta(
-	sistema int,
-  	sistema_galaxia int,
-  	planeta int,
-  	foreign key (sistema) references sistema(id_sistema),
-  	foreign key (sistema_galaxia) references galaxia(id_galaxia),
-  	foreign key (planeta) references planeta(id_planeta)
-);
-
 create table estrela(
-	id_estrela int,
+	id_estrela varchar(45) default 'temp',
   	nome varchar(45),
   	tamanho float,
   	idade int,
@@ -72,51 +44,95 @@ create table estrela(
   	primary key (id_estrela)
 );
 
+create table sistema_estrela(
+	sistema varchar(45),
+  	sistema_galaxia varchar(45),
+  	estrela varchar(45),
+  	constraint fk_sistema foreign key (sistema) references sistema(id_sistema),
+  	constraint fk_sistema_galaxia foreign key (sistema_galaxia) references galaxia(id_galaxia),
+  	constraint fk_estrela foreign key (estrela) references estrela(id_estrela)
+);
+
+create table sistema_planeta(
+	sistema varchar(45),
+  	sistema_galaxia varchar(45),
+  	planeta varchar(45),
+  	foreign key (sistema) references sistema(id_sistema),
+  	foreign key (sistema_galaxia) references galaxia(id_galaxia),
+  	foreign key (planeta) references planeta(id_planeta)
+);
+
 create table ana_vermelha(
-	estrela int,
+	estrela varchar(45),
   	foreign key (estrela) references estrela(id_estrela)
 );
 
 create table ana_branca(
-	estrela int,
+	estrela varchar(45),
   	foreign key (estrela) references estrela(id_estrela)
 );
 
 create table gigante_azul(
-	estrela int,
+	estrela varchar(45),
   	foreign key (estrela) references estrela(id_estrela)
 );
 
 create table estrela_binaria(
-	estrela int,
+	estrela varchar(45),
   	foreign key (estrela) references estrela(id_estrela)
 );
 
 create table gigante_vermelha(
-	estrela int,
+	estrela varchar(45),
   	morte varchar(45),
   	primary key (estrela),
   	foreign key (estrela) references estrela(id_estrela)
 );
 
 create table buraco_negro(
-	gigante_vermelha int,
+	gigante_vermelha varchar(45),
   	foreign key (gigante_vermelha) references gigante_vermelha(estrela)
 );
 
+
+create table satelite(
+	id_satelite varchar(45) default 'temp',
+  	nome varchar(45),
+  	tamanho float,
+  	peso float,
+  	comp_sn varchar(45),
+    constraint pk_satelite primary key (id_satelite)
+);
+
 create table orbitar(
-	satelite int,
-  	planeta int,
-  	estrela int,
+	satelite varchar(45),
+  	planeta varchar(45),
+  	estrela varchar(45),
   	foreign key (satelite) references satelite(id_satelite),
   	foreign key (planeta) references planeta(id_planeta),
   	foreign key (estrela) references estrela(id_estrela)
 );
 
-create table satelite(
-	id_satelite int,
-  	nome varchar(45),
-  	tamanho float,
-  	peso float,
-  	comp_sn varchar(45)
-);
+create function proxima_chave() returns trigger as $definir_chave$
+    declare
+        nome_tabela varchar(45) := TG_TABLE_NAME;
+        pk varchar(45) := TG_TABLE_NAME;
+        pk_id varchar(45) := TG_TABLE_NAME;
+        seq varchar(45) := TG_TABLE_NAME;
+        def varchar(4) := 'temp';
+        run_seq_num integer := 30;
+    begin
+        seq := seq || '_seq';
+        select nextval(seq) into run_seq_num;
+        pk := pk || run_seq_num;
+        pk_id := 'id_' || pk_id;
+        execute 'update ' || nome_tabela || ' set ' || pk_id || ' = ''' || pk || ''' where ' || pk_id || ' = ''temp''';
+        return new;
+    end;
+$definir_chave$ language plpgsql;
+
+create trigger definir_chave after insert on planeta for each row execute procedure proxima_chave();
+create trigger definir_chave after insert on estrela for each row execute procedure proxima_chave();
+create trigger definir_chave after insert on galaxia for each row execute procedure proxima_chave();
+create trigger definir_chave after insert on sistema for each row execute procedure proxima_chave();
+create trigger definir_chave after insert on satelite for each row execute procedure proxima_chave();
